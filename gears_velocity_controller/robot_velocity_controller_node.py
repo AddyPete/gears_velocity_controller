135c857c6d2acbae84ffe074a9cab20928713949
Adrian Pete Lagare <aprlagare1999@gmail.com>
2024-02-16 11:47:35 +0800

    Added two drive modes: AFRS and Differential Drive

--- gears_velocity_controller/robot_velocity_controller_node.py
+++ gears_velocity_controller/robot_velocity_controller_node.py
@@ -12,25 +12,45 @@ class VelocityController(Node):
         self.declare_parameter("serial_port", "/dev/ttyUSB0")
         self.declare_parameter("baudrate", 115200)
         self.declare_parameter("cmd_vel_topic", "/cmd_vel")
+        self.declare_parameter("drive_mode", "AFRS")
 
         self._serial_port = self.get_parameter("serial_port").value
         self._baud_rate = self.get_parameter("baudrate").value
         self._cmd_vel_topic = self.get_parameter("cmd_vel_topic").value
+        self._drive_mode = self.get_parameter("drive_mode").value
 
         self._serial_connection = serial.Serial(self._serial_port, self._baud_rate)
 
-        self._gears_cmd_vel = Twist()
+        self._command_string = [0.0, 0.0]
 
         self._serial_velocity_broadcaster = SerialVelocityBroadcaster(
-            self._serial_connection, self._gears_cmd_vel
+            self._serial_connection, self._command_string
         )
 
         self.create_subscription(Twist, self._cmd_vel_topic, self.twist_callback, 10)
 
     def twist_callback(self, twist_msg):
-        self._gears_cmd_vel.linear.x = twist_msg.linear.x
-        self._gears_cmd_vel.angular.z = twist_msg.angular.z
-        self._serial_velocity_broadcaster.broadcast_velocity()
+
+        linear_velocity = twist_msg.linear.x
+        angular_velocity = twist_msg.angular.z
+
+        if self._drive_mode == "AFRS":
+            self._command_string[0] = linear_velocity
+            self._command_string[1] = angular_velocity
+            self._serial_velocity_broadcaster.broadcast_velocity()
+
+        elif self._drive_mode == "differential":
+            left_motor_velocity = linear_velocity - angular_velocity
+            right_motor_velocity = linear_velocity + angular_velocity
+
+            self._command_string[0] = left_motor_velocity
+            self._command_string[1] = right_motor_velocity
+            self._serial_velocity_broadcaster.broadcast_velocity()
+
+        else:
+            self.get_logger().info(
+                "Invalid Type | Please choose between AFRS or differential only"
+            )
 
 
 def main(args=None):
