7bbf4f609dfee60c1e654bb574e52328e1ff2742
Adrian Pete Lagare <aprlagare1999@gmail.com>
2024-03-01 14:02:45 +0800

    Added mode switching capability

--- gears_velocity_controller/robot_velocity_controller_node.py
+++ gears_velocity_controller/robot_velocity_controller_node.py
@@ -1,8 +1,13 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+from sensor_msgs.msg import Joy
 from SerialCommunication import SerialVelocityBroadcaster
 import serial
+
+ROTATION_INCREMENT = 0.008
+MAX_STEER = 0.3
 
 
 class VelocityController(Node):
@@ -21,22 +26,49 @@ class VelocityController(Node):
 
         self._serial_connection = serial.Serial(self._serial_port, self._baud_rate)
 
-        self._command_string = [0.0, 0.0]
+        self._command_string = [0.0, 0.0, 0]
+
+        self._mode = 0  # 4WS
 
         self._serial_velocity_broadcaster = SerialVelocityBroadcaster(
             self._serial_connection, self._command_string, self._drive_mode
         )
 
+        self._mode_publisher = self.create_publisher(String, "/mode", 10)
+
+        self.create_subscription(Joy, "joy", self.joy_callback, 10)
         self.create_subscription(Twist, self._cmd_vel_topic, self.twist_callback, 10)
+        self.create_subscription(String, "/mode", self.mode_callback, 10)
 
     def twist_callback(self, twist_msg):
 
         linear_velocity = twist_msg.linear.x
         angular_velocity = twist_msg.angular.z
 
+        self.get_logger().info(f"Angular Velocity: {angular_velocity}")
+        self.get_logger().info(f"Drive Mode: {self._drive_mode}")
+
         if self._drive_mode == "4WS":
             self._command_string[0] = linear_velocity
-            self._command_string[1] = angular_velocity
+
+            if angular_velocity > 0:
+
+                check_val = self._command_string[1]
+                check_val -= ROTATION_INCREMENT
+                if check_val >= -MAX_STEER:
+                    self._command_string[1] -= ROTATION_INCREMENT
+            elif angular_velocity < 0:
+
+                check_val = self._command_string[1]
+                check_val += ROTATION_INCREMENT
+                if check_val <= MAX_STEER:
+                    self._command_string[1] += ROTATION_INCREMENT
+
+            else:
+                self._command_string[1] = 0
+
+            self.get_logger().info(f"Broadcast")
+
             self._serial_velocity_broadcaster.broadcast_velocity()
 
         elif self._drive_mode == "differential":
@@ -51,6 +83,31 @@ class VelocityController(Node):
             self.get_logger().info(
                 "Invalid Type | Please choose between 4WS or differential only"
             )
+
+    def joy_callback(self, joy_msg):
+
+        if joy_msg.axes[7] == 1:
+            self.publish_mode("4ws")
+
+        # Check the 7th axis (index 6)
+        elif joy_msg.axes[6] == -1:
+            self.publish_mode("spin")
+
+    def publish_mode(self, mode):
+
+        self._mode_publisher.publish(String(data=mode))
+        self.get_logger().info(f'Publishing mode: "{mode}"')
+
+    def mode_callback(self, msg):
+        # This function is called when a new message is received on the /mode topic
+        mode = msg.data  # Extract the data (string) from the message
+
+        if mode.lower() == "4ws":
+            self._mode = 0
+        elif mode.lower() == "spin":
+            self._mode = 1
+
+        self.get_logger().info(f'Received mode: "{mode} | VAL: {self._mode}"')
 
 
 def main(args=None):
